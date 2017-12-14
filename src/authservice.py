134e188c2debd403cb2efd4925e9422eb597db4b
Dean Taylor <dean@gohitech.net>
2017-12-14 20:04:54 +0800

    workaround, clear new user permissions

--- src/authservice.py
+++ src/authservice.py
@@ -122,13 +122,6 @@ class AuthService():
         user.last_name = user_dict.get('last_name', '')
 
         user.save()
-        user.user_permissions.add(Permission.objects.get(codename='add_experiment'))
-        user.user_permissions.add(Permission.objects.get(codename='change_experiment'))
-        user.user_permissions.add(Permission.objects.get(codename='change_group'))
-        user.user_permissions.add(Permission.objects.get(codename='change_userauthentication'))
-        user.user_permissions.add(Permission.objects.get(codename='change_objectacl'))
-        user.user_permissions.add(Permission.objects.get(codename='add_datafile'))
-        user.user_permissions.add(Permission.objects.get(codename='change_dataset'))
 
     def get_or_create_user(self, user_obj_or_dict, authMethod=None):
         '''
